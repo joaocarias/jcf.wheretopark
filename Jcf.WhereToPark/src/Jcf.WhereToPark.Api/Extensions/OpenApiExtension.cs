05b73cad9f9aa48444a111e3627b73ca7b42289e
Joao Carias de Franca <joaocariasdefranca@gmail.com>
2025-05-29 23:57:31 -0300

    Adicinando OpenApi e Swagger

---
+++ Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Extensions/OpenApiExtension.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace Jcf.WhereToPark.Api.Extensions
+{
+    public static class OpenApiExtension
+    {
+        public static IServiceCollection AddCustomOpenApi(this IServiceCollection services)
+        {
+            services.AddOpenApi(
+                 options =>
+                 {
+                     options.AddDocumentTransformer((document, context, cancellationToken) =>
+                     {
+                         document.Info.Contact = new OpenApiContact
+                         {
+                             Name = "Where To Park",
+                             Email = "contact@wheretopark.com"
+                         };
+
+                         return Task.CompletedTask;
+                     });
+
+                     options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+                 });
+
+            return services;
+        }
+    }
+
+    internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+    {
+        public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+        {
+            var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+            if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+            {
+                // Add the security scheme at the document level
+                var requirements = new Dictionary<string, OpenApiSecurityScheme>
+                {
+                    ["Bearer"] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer", // "bearer" refers to the header name here
+                        In = ParameterLocation.Header,
+                        BearerFormat = "Json Web Token"
+                    }
+                };
+                document.Components ??= new OpenApiComponents();
+                document.Components.SecuritySchemes = requirements;
+
+                // Apply it as a requirement for all operations
+                foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
+                {
+                    operation.Value.Security.Add(new OpenApiSecurityRequirement
+                    {
+                        [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
+                    });
+                }
+            }
+        }
+    }
+}
--- Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Program.cs
+++ Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Program.cs
@@ -1,10 +1,26 @@
+using Jcf.WhereToPark.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine(builder.Configuration.GetSection("EnvironmentName").Value);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddCustomOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("v1");
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
 
 // Configure the HTTP request pipeline.
 
@@ -13,5 +29,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapOpenApi();
 
 app.Run();
