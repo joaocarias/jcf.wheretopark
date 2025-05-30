4b1d5ecd0c83c57f9055a499ac79fef3336ac7f1
João França <jfranca@omnize.com.br>
2025-05-30 16:46:39 -0300

    Adicinando Controller Base

---
+++ Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Core/Controllers/AppControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Jcf.WhereToPark.Api.Core.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jcf.WhereToPark.Api.Core.Controllers
+{
+    public class AppControllerBase : ControllerBase
+    {
+        protected Guid? GetUserIdFromToken()
+        {
+            try
+            {
+                var userId = (HttpContext.User.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypeConstants.USER_ID));
+                if (userId is null || !Guid.TryParse(userId.Value, out var id))
+                    return null;
+                return id;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{nameof(AppControllerBase)} | {nameof(GetUserIdFromToken)}: " + ex.ToString());
+            }
+
+            return null;
+        }
+    }
+}
