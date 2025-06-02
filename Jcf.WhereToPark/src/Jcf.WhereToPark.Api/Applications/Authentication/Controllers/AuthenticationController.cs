e7bbdb2ca4ff973802381c314e821de8655f48cf
João França <jfranca@omnize.com.br>
2025-06-02 16:23:41 -0300

    Iniciciando Login

---
+++ Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Applications/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using Jcf.WhereToPark.Api.Applications.Authentication.Models.DTOs;
+using Jcf.WhereToPark.Api.Applications.User.Controllers;
+using Jcf.WhereToPark.Api.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jcf.WhereToPark.Api.Applications.Authentication.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : AppControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public AuthenticationController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(LoginDTO loginDTO)
+        {
+            try
+            {
+                if (!loginDTO.IsValidate())
+                {
+
+                }
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }            
+        }
+
+
+    }
+}
---
+++ Jcf.WhereToPark/src/Jcf.WhereToPark.Api/Applications/Authentication/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace Jcf.WhereToPark.Api.Applications.Authentication.Models.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public LoginDTO()
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+        }
+
+        public LoginDTO(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public bool IsValidate()
+        {
+            if(string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(Password))
+            {
+                throw new ArgumentNullException($"{nameof(Email)} and {nameof(Password)} cannot be null or empty");
+            }
+
+            return true;
+        }
+    }
+}
